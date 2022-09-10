160bb34ff3f0ca3834009b8d408a7eb996b2fd47
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-10 11:47:33 +0200

    Init

---
+++ src/main/java/com/skillsoft/springinitalizer/SpringinitalizerApplication.java
@@ -0,0 +1,13 @@
+package com.skillsoft.springinitalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinitalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinitalizerApplication.class, args);
+	}
+
+}
